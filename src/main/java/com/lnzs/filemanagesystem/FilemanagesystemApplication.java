186b71407dc80de758e8a10835c619f2b54519e3
sherlock <1347997728@qq.com>
2019-07-11 08:30:32 +0800

    创建项目

---
+++ src/main/java/com/lnzs/filemanagesystem/FilemanagesystemApplication.java
@@ -0,0 +1,13 @@
+package com.lnzs.filemanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilemanagesystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilemanagesystemApplication.class, args);
+    }
+
+}
